e6ac731fae832a60b15d852e8b17457cd266542a
Linta Paul <54324010+LintaPaul@users.noreply.github.com>
2020-09-19 18:59:55 +0530

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <Nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+  color: #2E495E;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.container {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background: url("../assets/bisup.jpg");
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-blend-mode: blend;
+}
+
+.button--green {
+  display: inline-block;
+  color: white;
+  text-decoration: none;
+  padding: 10px 30px;
+  display: inline-flex;
+  align-self: center;
+  opacity: 100%;
+  background: #33499A;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  opacity: 75%;
+}
+
+.button--grey {
+  display: inline-block;
+  color: white;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+  background: #33499A;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 30px;
+  opacity: 100%;
+}
+
+.button--grey:hover {
+  color: #fff;
+  opacity: 75%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,89 @@
+<template >
+  <section>
+    <div>
+      <div>
+        <h2 class="titles">BiZUp</h2>
+        <h4 class="subtitle">A one stop solution to all your business needs</h4>
+      </div>
+      <div class="links">
+        <u class="txt">New user?</u>
+        <NLink to="/register" class="button--green">Register</NLink>&emsp;
+        <u class="txt">Already a user?</u>
+        <NLink to="/feeds" class="button--grey">Login</NLink>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import IconLink from "~/components/IconLink.vue";
+
+export default {
+  components: {
+    Logo,
+    IconLink
+  }
+};
+</script>
+
+<style scoped>
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 400;
+  font-size: 100px;
+  color: #2E495E;
+  letter-spacing: 1px;
+  font-size: 6em;
+}
+.green {
+  color: #00C48D;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 3em;
+  color: #2E495E;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+* {
+  font-family: Roboto;
+}
+.titles {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 120px;
+  line-height: 160px;
+  text-align: center;
+
+  color: #FFFFFF;
+
+  backdrop-filter: blur(4px);
+}
+.subtitle {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 36px;
+  line-height: 42px;
+  text-align: center;
+
+  color: #FFFFFF;
+}
+.txt {
+  display: inline-block;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  text-align: justify;
+  text-decoration-line: underline;
+
+  color: #FFFFFF;
+}
+</style>
